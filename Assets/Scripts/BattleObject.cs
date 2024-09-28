3603156956b2a7d65ae8b42dd5fd0523a4053d75
osuCARL <95015759+osuCarl@users.noreply.github.com>
2024-09-29 07:50:45 +1000

    add battleObject

---
+++ Assets/Scripts/BattleObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class BattleObject : MonoBehaviour
+{
+    public string battleObjectName;
+    public string description;
+    public int currentHP;
+    public int maxHP;
+    public int level;
+    public int damage;
+    public int defense;
+    public int exp;
+    public int gold;
+}
