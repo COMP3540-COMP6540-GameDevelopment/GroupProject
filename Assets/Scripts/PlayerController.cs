bd1bb774be358f873d0fec8ea1f3cf581d5f988f
osuCARL <95015759+osuCarl@users.noreply.github.com>
2024-10-02 15:22:38 +1000

    add double jump feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,39 +28,21 @@ public class PlayerController : MonoBehaviour
         // Movement
         moveAction.Enable();
         playerRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("f_Move_X", moveDirection);
+        animator.SetFloat("f_Speed", moveSpeed);
 
         //Jump
         jumpAction.Enable();
         isJump = false;
 
-        // Animator
-        animator = GetComponent<Animator>();
-        animator.SetFloat("f_Move_X", moveDirection);
-        animator.SetFloat("f_Speed", moveSpeed);
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        move = moveAction.ReadValue<Vector2>(); // Read player movement input
-        
-        jumpAction.performed += Jump;   // bind Jump() method to this action
-
-        if (isJump)
-        {
-            animator.SetFloat("f_Move_Y", playerRb.velocity.y);
-            if (Mathf.Approximately(playerRb.velocity.y, 0))
-            {
-                isJump = false;
-                animator.SetBool("b_isJump", isJump);
-                animator.SetFloat("f_Move_Y", 0);
-            }
-        }
-
-        // Set animator state
+        // Related to movement
+        move = moveAction.ReadValue<Vector2>();
         if (!Mathf.Approximately(move.x, 0))
         {
             moveDirection = move.x;
@@ -72,35 +54,58 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat("f_Speed", 0f);
         }
 
-        Debug.Log(playerRb.velocity);
+        // Related to jump
+        jumpAction.performed += Jump;   // bind Jump() method to this action
+        if (isJump)
+        {
+            animator.SetFloat("f_Move_Y", playerRb.velocity.y);
+            if (Mathf.Approximately(playerRb.velocity.y, 0))
+            {
+                isJump = false;
+                animator.SetBool("b_isJump", isJump);
+                animator.SetFloat("f_Move_Y", 0);
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        // Related to movement
         Vector2 position = playerRb.position + speed * Time.deltaTime * move;
         playerRb.position = position;
     
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         CompositeCollider2D platform = collision.gameObject.GetComponent<CompositeCollider2D>();
 
         if (platform != null)
         {
-            // Player stops collide with the platform
-            
+            // Player collides with the platform
+            jumpAction.Enable();
+            isJump = false;
+            animator.SetBool("b_isJump", isJump);
+            animator.SetFloat("f_Move_Y", 0);
         }
     }
 
     void Jump(InputAction.CallbackContext callbackContext)
     {
-        isJump = true;
-        playerRb.velocity = new Vector2(playerRb.velocity.x, jumpVelocity);
+        if (isJump)
+        {
+            // Double jump
+            jumpAction.Disable();
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpVelocity);
+            animator.SetTrigger("t_DoubleJump");
+        } 
+        else
+        {
+            isJump = true;
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpVelocity);
+            animator.SetBool("b_isJump", isJump);
+        }
 
-        animator.SetFloat("f_Move_Y", playerRb.velocity.y);
-        animator.SetBool("b_isJump", isJump);
     }
     
 }
